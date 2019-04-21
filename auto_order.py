449100bb139b4f42aec0b16febe8da4a3cba5f6f
dengbb <804433988@qq.com>
2019-04-21 21:23:26 +0900

    url config

--- auto_order.py
+++ auto_order.py
@@ -7,30 +7,20 @@ import time
 
 
 
-url = 'https://kiba-park.revn.jp/kiba-bbq/reserve/add_not_user?id=716f326833e86519e4ee20616868e438&event_id=27&date=2019-04-24&time=10%3A00'
-
-select_num = 2
-people_num = '20'
-order_date = '2019-04-24'
-family_name = 'deng'
-first_name = 'bing'
-tel1 = '080'
-tel2 = '9980'
-tel3 = '0801'
+#平常是使用configparser.ConfigParser()来读取配置文件，但是当配置文件有特殊字符时就需要另外一个类了configparser.RawConfigParser()
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 info_list = list(config.items('INFO_LIST'))
 
-
-
-
-
+config_for_url = configparser.RawConfigParser()
+config_for_url.read('config.ini')
+url = config_for_url.get('URL','url')
+print url
 driver = webdriver.Chrome()
 driver.get(url)
 
 name_list = ["reserve[number]", "reserve[value_31]", "reserve[value_28][last_name]","reserve[value_28][first_name]","reserve[value_29][number_1]","reserve[value_29][number_2]","reserve[value_29][number_3]"]
-
 policy_id = 'reserve_policy'
 confirm_xpatch= '//*[@id="form"]/div[6]/input[2]'
 login_xpatch = '/html/body/div/section/div/div[2]/div[2]/form/div[3]/input[2]'
@@ -42,11 +32,11 @@ def auto_order():
         for i in xrange(0,7):
             dom_name = name_list[i]
             content_name = info_list[i][1]
-            content_name = content_name.encode('ascii','ignore')
+
             content_name = content_name.replace('\'','')
             if i == 0:
                 select_item = Select(driver.find_element_by_name(dom_name))
-                select_item.select_by_index(select_num)
+                select_item.select_by_index(content_name)
             
             seletem_item = driver.find_element_by_name(dom_name)
             seletem_item.send_keys(content_name) 
@@ -60,7 +50,6 @@ def auto_order():
         # driver.find_element_by_xpath(login_xpatch).click()
     except Exception as e:
         print e
-
 
 if __name__ == "__main__":
     auto_order()
