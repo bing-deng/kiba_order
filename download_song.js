bf0c487e8995245215b0fb2f8a17860fe3b5838f
dengbb <804433988@qq.com>
2019-06-11 18:28:33 +0900

    js

---
+++ download_song.js
@@ -0,0 +1,16 @@
+// song website https://www.mp3juices.cc/
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+// 用法
+sleep(500).then(() => {
+    // 这里写sleep之后需要去做的事情
+})
+await sleep(1500)
+$('input[name="query"]')[0].value = "perfet"
+$('#button').click()
+await sleep(1500);
+$('.download.1')[0].click()
+await sleep(1500)
+$('.url')[0].click()
